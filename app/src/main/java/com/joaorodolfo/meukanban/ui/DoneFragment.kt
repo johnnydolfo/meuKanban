2f1a3a4b22092ecc044ee5c09d71783db81ff06c
johnnydolfo <126531124+johnnydolfo@users.noreply.github.com>
2025-09-03 22:30:24 -0300

    Add files via upload

---
+++ app/src/main/java/com/joaorodolfo/meukanban/ui/DoneFragment.kt
@@ -0,0 +1,82 @@
+package com.anaju.task.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.anaju.task.R
+import com.anaju.task.data.model.Status
+import com.anaju.task.data.model.Task
+import com.anaju.task.databinding.FragmentDoneBinding
+import com.anaju.task.ui.adapter.TaskAdapter
+
+class DoneFragment : Fragment() {
+
+    private var _binding: FragmentDoneBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var taskAdapter: TaskAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDoneBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+        initRecyclerViewTask(getTask())
+    }
+
+    private fun initListeners(){
+        binding.floatingActionButton2.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_formTaskFragment)
+        }
+    }
+
+    private fun initRecyclerViewTask(taskList: List<Task>){
+        taskAdapter = TaskAdapter(requireContext(), taskList) {task, option -> optionSelected(task,option)}
+        binding.recyclerViewTask.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerViewTask.setHasFixedSize(true)
+        binding.recyclerViewTask.adapter = taskAdapter
+    }
+
+    private fun optionSelected(task:Task, option: Int){
+        when (option){
+            TaskAdapter.SELECT_REMOVER -> {
+                Toast.makeText(requireContext(), "Removendo ${task.description}", Toast.LENGTH_SHORT).show()
+            }
+            TaskAdapter.SELECT_EDIT -> {
+                Toast.makeText(requireContext(), "Editando ${task.description}", Toast.LENGTH_SHORT).show()
+            }
+            TaskAdapter.SELECT_DETAILS -> {
+                Toast.makeText(requireContext(), "Detalhes ${task.description}", Toast.LENGTH_SHORT).show()
+            }
+            TaskAdapter.SELECT_BACK -> {
+                Toast.makeText(requireContext(), "Anterior", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun getTask() = listOf(
+        Task("0", "Adicionar funcionalidade de backup de dados na nuvem", Status.DONE),
+        Task("1","Integrar sistema de feedback de usuários", Status.DONE),
+        Task("2","Criar tela de tutorial inicial para novos usuários", Status.DONE),
+        Task("3", "Criar fluxo de recuperação de senha", Status.DONE),
+        Task("4","Desenvolver tela de histórico de atividades", Status.DONE),
+    )
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
